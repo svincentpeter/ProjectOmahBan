790f76e6986cffb2a1bc920d248bfd45cb4e1619
Vincent Peter <vincentpeter@gmail.com>
2025-08-05 20:50:38 +0700

    Upload Semua File dari Project

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,70 @@
+<?php
+
+if (!function_exists('settings')) {
+    function settings() {
+        $settings = cache()->remember('settings', 24*60, function () {
+            return \Modules\Setting\Entities\Setting::firstOrFail();
+        });
+
+        return $settings;
+    }
+}
+
+if (!function_exists('format_currency')) {
+    function format_currency($value, $format = true) {
+    if (!$format) {
+        return $value;
+    }
+
+    $settings = settings();
+    $currency = $settings->currency;
+
+    // Cegah error jika currency null
+    if (is_null($currency)) {
+        return number_format((float) $value, 2, ',', '.'); // fallback format
+    }
+
+    $position = $settings->default_currency_position;
+    $symbol = $currency->symbol;
+    $decimal_separator = $currency->decimal_separator;
+    $thousand_separator = $currency->thousand_separator;
+
+    if ($position == 'prefix') {
+        $formatted_value = $symbol . number_format((float) $value, 2, $decimal_separator, $thousand_separator);
+    } else {
+        $formatted_value = number_format((float) $value, 2, $decimal_separator, $thousand_separator) . $symbol;
+    }
+
+    return $formatted_value;
+}
+
+}
+
+if (!function_exists('make_reference_id')) {
+    function make_reference_id($prefix, $number) {
+        $padded_text = $prefix . '-' . str_pad($number, 5, 0, STR_PAD_LEFT);
+
+        return $padded_text;
+    }
+}
+
+if (!function_exists('array_merge_numeric_values')) {
+    function array_merge_numeric_values() {
+        $arrays = func_get_args();
+        $merged = array();
+        foreach ($arrays as $array) {
+            foreach ($array as $key => $value) {
+                if (!is_numeric($value)) {
+                    continue;
+                }
+                if (!isset($merged[$key])) {
+                    $merged[$key] = $value;
+                } else {
+                    $merged[$key] += $value;
+                }
+            }
+        }
+
+        return $merged;
+    }
+}
